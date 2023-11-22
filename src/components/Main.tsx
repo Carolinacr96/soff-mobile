fd7e481ecdd5e75fe6500edbf8d1572eca2ddfe9
carolinacr96 <ccastro599@misena.edu.co>
2023-11-22 08:53:23 -0500

    bgcolor

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ import { View } from "native-base"
 
 export default function Main(){
     return(
-        <View bg="blue.100" style={{marginTop:Constants.statusBarHeight, flexGrow: 1 }}>
+        <View style={{marginTop:Constants.statusBarHeight, flexGrow: 1 }}>
             <Sales/>
         </View>
     )
