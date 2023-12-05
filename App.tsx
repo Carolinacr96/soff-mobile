37750d87c8e1c2a21b757624a3be6fc771b48758
carolinacr96 <ccastro599@misena.edu.co>
2023-12-05 13:41:02 -0500

    app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Main from './src/components/Main'
 import React from 'react'
-import { NativeBaseProvider, View } from 'native-base';
 
 export default function App() {
  return (
-   <NavigationContainer>
-     <Main />
+   <NavigationContainer >
+      <Main />
    </NavigationContainer>
  );
 }
